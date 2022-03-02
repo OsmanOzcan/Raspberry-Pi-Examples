75cefdd7d217de929660ba33c92811af3a3129b5
Osman Özcan <73670053+OsmanOzcan@users.noreply.github.com>
2022-03-03 01:38:23 +0300

    Add files via upload

---
+++ Motor/BrushlessMotorServoBlaster/brushlessMotor2.c
@@ -0,0 +1,25 @@
+// Install => git clone https://github.com/richardghirst/PiBits
+// Compile => gcc servo.c -lwiringPi -o servo
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+int main(){
+  
+ 	system("sudo /home/pi/Desktop/PiBits/ServoBlaster/user/servod --p1pins=11,13");
+  
+	
+ 	system("echo P1-11=50 > /dev/servoblaster");
+	system("echo P1-13=50 > /dev/servoblaster");
+	delay(7000);
+	
+  	while(1){
+
+    		system("echo P1-11=120 > /dev/servoblaster");
+		system("echo P1-13=120 > /dev/servoblaster");
+  	}
+  
+  	return 0;
+}
