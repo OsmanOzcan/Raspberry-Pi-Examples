4a70257b26b973b4a9c0759c3313a58158294cdf
Osman Özcan <73670053+OsmanOzcan@users.noreply.github.com>
2022-03-02 19:53:17 +0300

    Add files via upload

---
+++ Sensors/ina219/ina219test.c
@@ -0,0 +1,28 @@
+//Source => https://github.com/hepingood/ina219.git
+//Compile => gcc ina219test.c raspberrypi4b_driver_ina219_interface.c iic.c driver_ina219_basic.c driver_ina219.c -lm  -o ina219test
+
+
+#include "driver_ina219_basic.h"
+#include "iic.h"
+#include <stdio.h>
+
+int main(){
+	
+    
+    
+    ina219_basic_init(INA219_ADDRESS_0, 0.1);
+                      
+                        
+                        while(1)
+                        {
+                            float mA;
+                            
+                            ina219_basic_read(&mA);
+                            
+                            printf("%f\n", mA);
+                        }
+                        
+                        ina219_basic_deinit();
+
+	return 0;
+}
