ec32b7319634f58a5bcad1a5912b5d00d331fb0f
Osman Özcan <73670053+OsmanOzcan@users.noreply.github.com>
2022-03-02 19:50:00 +0300

    Update bno055.c

--- Sensors/bno055.c
+++ Sensors/bno055.c
@@ -1,5 +1,5 @@
 // Download library to /usr/include => git clone https://github.com/fm4dd/pi-bno055.git
-// Compile => gcc bno055test.c /usr/include/pi-bno055/i2c_bno055.c -o bno055test
+// Compile => gcc bno055.c /usr/include/pi-bno055/i2c_bno055.c -o bno055
 
 #include <stdio.h>
 #include <pi-bno055/getbno055.h>
